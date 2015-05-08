aa80f3556afbaa2593109932ea8e6cdd19070082
Michael Mitchell <michaelmatthewmitchell@live.com>
2015-05-08 13:29:30 -0700

    Commented Dijkstra's implementation

--- src/data/AirportGraph.java
+++ src/data/AirportGraph.java
@@ -24,62 +24,87 @@ public class AirportGraph {
 	public Airport[] CalculateRoute(Airport start, Airport end){
 		if(start.code().equals(end.code())) return new Airport[]{start};
 		
+		// set up containers to hold nodes
+		
+		// d holds the distance from the start node to each node
 		HashMap<Airport, Float>   d = new HashMap<Airport, Float>();
+		
+		// p holds the previous node in the path for each node 
 		HashMap<Airport, Airport> p = new HashMap<Airport, Airport>();
 		
-		HashMap<Airport, Float> S = new HashMap<Airport, Float>();
+		// S holds already searched nodes
+		ArrayList<Airport> S = new ArrayList<Airport>();
 		
+		// R holds nodes that still need to be pathed to
 		ArrayList<Airport> R = new ArrayList<Airport>();
 		
-		S.put(start, 0.0f);
+		// put the starting node in S
+		S.add(start);
+		
+		// put the starting node in p and set its previous node to null
 		p.put(start, null);
 		
+		// get all the nodes that connect to the starting node
 		ArrayList<Airport> start_con = new ArrayList<Airport>();
 		for(Airport apt : routes_.get(start)) start_con.add(apt);
 		
+		// initialize R with all nodes that are not the starting node
+		// if the current node being iterated on is connected to the starting node
+		//   put that node in d and set the distance equal to the edge between it and the starting node
+		// else
+		//   put the current node in d but set its distance to the starting node to pos infinity
 		for(Airport apt : airports_){
 			if(!apt.code().equals(start.code())){
 				int ind;
+				R.add(apt);
 				if((ind = start_con.indexOf(apt)) != -1){
-					R.add(apt);
 					d.put(apt, dists_.get(start)[ind]);
 				}
 				else{
-					R.add(apt);
 					d.put(apt, Float.POSITIVE_INFINITY);
 				}
 			}
 		}
-			
 		
+		// while there are nodes to search over
 		while(!R.isEmpty()){
-			Airport[] lowest_dist = new Airport[]{d.keySet().iterator().next()};
-			BiConsumer<Airport, Float> bc0 = (x, y) -> {
-				if(d.get(x) < d.get(lowest_dist[0])){
-					lowest_dist[0] = x;
-				}
-			};
-			d.forEach(bc0);
-			R.remove(lowest_dist[0]);
 			
-			if(R.contains(lowest_dist[0])){
-				System.out.println(R.contains(lowest_dist[0]));
-			}
+			// find the node with the lowest distance from the starting node
+			Airport lowest_dist = R.get(0);
+			for(Airport apt : R)
+				if(d.get(apt) < d.get(lowest_dist))
+					lowest_dist = apt;
 			
-			S.put(lowest_dist[0], d.get(lowest_dist[0]));
+			// remove lowest_dist from S (nodes to search)
+			R.remove(lowest_dist);
 			
+			// print out the number of nodes left to search
+			//System.out.println(R.size());
+			
+			// add lowest_dist to S (already searched nodes)
+			S.add(lowest_dist);
+			
+			// loop over nodes that are connected to lowest_dist
+			// who are also still in S
 			int index = 0;
-			for(Airport apt : routes_.get(lowest_dist[0])){
+			for(Airport apt : routes_.get(lowest_dist)){
 				if(R.contains(apt)){
-					if(d.get(lowest_dist[0]) + dists_.get(lowest_dist[0])[index] < d.get(apt)){
-						d.put(apt, d.get(lowest_dist[0]) + dists_.get(lowest_dist[0])[index]);
-						p.put(apt, lowest_dist[0]);
+					// IF the distance from starting node to apt (current node)
+					//    is shorter if you go through lowest_dist than it is currently
+					// THEN
+					//	set the distance of apt to the shorter distance, place in d
+					//  set the previous node for apt to lowest_dist
+					// ELSE continue loop
+					if(d.get(lowest_dist) + dists_.get(lowest_dist)[index] < d.get(apt)){
+						d.put(apt, d.get(lowest_dist) + dists_.get(lowest_dist)[index]);
+						p.put(apt, lowest_dist);
 					}
 				}
 				++index;
 			}
 		}
 		
+		// build arraylist that contains the path from end node to starting node
 		ArrayList<Airport> route = new ArrayList<Airport>();
 		Airport current = end;
 		while(current != null){
@@ -87,11 +112,11 @@ public class AirportGraph {
 			current = p.get(current);
 		}
 		
+		// put route into array where starting node is index 0
+		// and end node is last index
 		Airport[] ret = new Airport[route.size()];
 		for(int i = 0; i < route.size(); ++i){
 			ret[i] = route.get(route.size() - i - 1);
-			// TODO REMOVE
-			System.out.println(route.get(route.size() - i - 1));
 		}
 		
 		return ret;
